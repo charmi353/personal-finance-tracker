2f48bcb42052d58b623d8bf8ba8cbe13778d2d58
BORRA SRI CHARMI <charmisri353@gmail.com>
2026-07-27 23:17:21 +0530

    fixed socket url

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://personal-finance-tracker-brdf.onrender.com/api", {
     transports: ["websocket", "polling"],
     withCredentials: true,
 });
